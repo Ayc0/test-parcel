97513a555966b12e4f0786a00845cd02e2b6cfd4
Benjamin Koltes <ayc0.benj@gmail.com>
2022-11-12 04:22:16 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+import list from "./hello.preval";
+
+console.log(list);
+
+document.body.innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  We use the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel 
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
